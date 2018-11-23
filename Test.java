588c97708ef43a70e382ff9f2e59117cdf03e4d8
alecnarkizian <45223913+alecnarkizian@users.noreply.github.com>
2018-11-22 20:34:14 -0800

    Add files via upload

---
+++ Test.java
@@ -0,0 +1,12 @@
+//Program public test class, Lab 8
+//Written by Alec Narkizian
+//created on 7.12,2017
+//tests the actual class PhoneNumberFormat
+//calls the method phoneNum in PhoneNumberFormat 
+public class Test{
+   public static void main(String[] args){   
+      PhoneNumberFormat num = new PhoneNumberFormat();
+      num.phoneNum();
+      }
+      
+     }
